8d9ffb85a50b1842751ecb3984d2403881b95a08
Mikel Emaldi <m.emaldi@gmail.com>
2014-03-03 15:46:21 +0100

    Grafico del paro de cada pueblo

--- paro/views.py
+++ paro/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from paro.models import Municipio
 
 # Create your views here.
+
+def infoMunicipio(request, id_municipio):
+    municipio = Municipio.objects.get(id=id_municipio)
+    datos = municipio.datosparo_set.all()
+    context = {}
+    lista_datos = []
+    for dato in datos:
+        porcentaje_paro = round(float(dato.parados) / dato.poblacion_activa * 100, 2)
+        lista_datos.append((dato.ano, dato.trimestre, porcentaje_paro)) 
+    context['lista_datos'] = lista_datos
+    return render(request, 'paro/municipio.html', context)
+    
+def mapa(request):
+    municipios = Municipio.objects.all()
+    context = {}
+    for municipio in municipios:
+        datos_paro = municipio.datosparo_set.filter(ano=2013, trimestre=4)
+        
